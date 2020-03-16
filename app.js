730807055721d5e9779fb32f19cd5dce9b5d0a7c
Tan <tanamanda13@gmail.com>
2020-03-16 23:42:35 +0100

    Récupérer les paramètres du post

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 // const port = 3000;
 const dotenv = require('dotenv').config();
 app.set('view engine', 'pug');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'Accueil', name_app: 'Qui prend whaat ?' });
@@ -10,6 +12,7 @@ app.get('/', function(req, res) {
 
 // create a event
 app.post('/party', function(req, res) {
+  console.log(req.body);
   res.send('Post ok!');
 });
 
