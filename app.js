babf73c91cf17d1321ae244b018ecea0551c6781
Tan <tanamanda13@gmail.com>
2020-03-16 17:52:36 +0100

    Variables d'environnement

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+// const port = 3000;
+const dotenv = require('dotenv').config();
 
 app.get('/', function(req, res) {
   res.send('tp quiprendquoi');
 });
 
-app.listen(port, () => console.log(`Front app listening on port ${port}!`));
+app.listen(process.env.PORT, () => console.log(`Front app listening on process.env.PORT ${process.env.PORT}!`));
