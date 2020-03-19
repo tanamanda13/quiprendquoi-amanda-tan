7565a38d88065cfeb42dc90adab500eb85917f93
Tan <tanamanda13@gmail.com>
2020-03-19 22:21:51 +0100

    Utiliser SASS

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'Accueil', name_app: 'Qui prend whaat ?' });
