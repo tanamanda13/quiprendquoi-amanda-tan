e3a7f647bad9e303faebe37d2211dd0447a79ea0
Tan <tanamanda13@gmail.com>
2020-03-20 23:19:03 +0100

    Intercepter une requête

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
 addEventListener('install', (event) => {
   console.log('Hello from the service worker')
 });
+
+addEventListener('fetch', (event) => {
+  // console.log(event);
+  if (event.request.headers.get('Accept').includes('text/html')) {
+    // event.respondWith(new Response('Hello World'));
+    event.respondWith(fetch(event.request));
+  }
+});
