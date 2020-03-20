a3463d497fb632108d52d66d3021de8ba4e15e5d
Tan <tanamanda13@gmail.com>
2020-03-20 23:46:06 +0100

    Page offline

--- sw.js
+++ sw.js
@@ -1,11 +1,22 @@
 addEventListener('install', (event) => {
   console.log('Hello from the service worker')
+  event.waitUntil(
+    caches.open('offline').then((cache) => {
+      cache.add('offline.html');
+    })
+  );
 });
 
 addEventListener('fetch', (event) => {
   // console.log(event);
   if (event.request.headers.get('Accept').includes('text/html')) {
     // event.respondWith(new Response('Hello World'));
-    event.respondWith(fetch(event.request));
+    // event.respondWith(fetch(event.request));
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => caches.match('offline.html')
+      )
+    );
+   
   }
 });
