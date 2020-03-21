259c27e8db0a9811aa0be7a135bfa49a78a1e497
Tan <tanamanda13@gmail.com>
2020-03-21 13:06:05 +0100

    Stocker les pages événement

--- sw.js
+++ sw.js
@@ -14,9 +14,36 @@ addEventListener('fetch', (event) => {
     // event.respondWith(fetch(event.request));
     event.respondWith(
       fetch(event.request)
-        .catch(() => caches.match('offline.html')
-      )
+      .then((res) => {
+        if (isPartyPage(event.request.url)) {
+          const copy = res.clone();
+          caches
+            .open('parties')
+            .then((cache) => cache.put(event.request, copy));
+          return res;
+        } else {
+          return res;
+        }
+      })
+      .catch(() => {
+        if (isPartyPage(event.request.url)) {
+          return caches
+            .match(event.request)
+            .catch((err) => caches.match('offline.html'));
+        } else {
+          return caches.match('offline.html');
+        }
+      })
     );
    
   }
 });
+
+
+/**
+ *
+ * savoir si une page est une page événément
+ */
+function isPartyPage(url) {
+  return /party\/[a-zA-Z0-9]*$/.test(url);
+}
